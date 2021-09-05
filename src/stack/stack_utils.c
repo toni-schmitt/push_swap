22c98716616e3e57a1e73f10046ab27c401872bd
Toni Schmitt <tschmitt@student.42heilbronn.de>
2021-09-05 19:29:50 +0200

    sort stack functions in dedicated folder

--- src/stack/stack_utils.c
+++ src/stack/stack_utils.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   stack.c                                            :+:      :+:    :+:   */
+/*   stack_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tschmitt <tschmitt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/29 23:15:32 by tschmitt          #+#    #+#             */
-/*   Updated: 2021/09/01 21:23:46 by tschmitt         ###   ########.fr       */
+/*   Created: 2021/09/05 19:28:34 by tschmitt          #+#    #+#             */
+/*   Updated: 2021/09/05 19:28:52 by tschmitt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,50 +51,4 @@ int	get_stack_size(t_stack *stack)
 		stack = stack->next;
 	}
 	return (i);
-}
-
-/* Returns a pointer to the last element of stack */
-t_stack	*get_last_element(t_stack *stack)
-{
-	while (stack->next)
-		stack = stack->next;
-	return (stack);
-}
-
-int	stack_add_front(t_stack **stack, t_stack *new)
-{
-	t_stack	*tmp;
-
-	if (new == NULL || stack == NULL)
-		return (FALSE);
-	tmp = *stack;
-	*stack = (*stack)->next;
-	tmp->next = new;
-	new = tmp;
-	return (TRUE);
-}
-
-int	stack_add_back(t_stack **stack, t_stack *new)
-{
-	if (new == NULL)
-		return (FALSE);
-	if (*stack == NULL)
-	{
-		*stack = new;
-		return (TRUE);
-	}
-	get_last_element(*stack)->next = new;
-	return (TRUE);
-}
-
-t_stack	*new_stack_element(int data)
-{
-	t_stack	*new;
-
-	new = malloc(sizeof(*new));
-	if (new == NULL)
-		return (NULL);
-	new->data = data;
-	new->next = NULL;
-	return (new);
 }
