4e7a03f133364434def1ee020ed201bf656d2f23
Toni Schmitt <tschmitt@student.42heilbronn.de>
2021-09-10 16:26:24 +0200

    protect if statement

--- src/sorting/very_small_sort.c
+++ src/sorting/very_small_sort.c
@@ -6,7 +6,7 @@
 /*   By: tschmitt <tschmitt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 12:43:29 by tschmitt          #+#    #+#             */
-/*   Updated: 2021/09/09 16:10:06 by tschmitt         ###   ########.fr       */
+/*   Updated: 2021/09/09 18:26:19 by tschmitt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ void	very_small_sort(t_stack **a, t_stack **b)
 	(void)b;
 	while (!is_sorted(*a))
 	{
-		if ((*a)->elements->data > (*a)->elements->next->data)
+		if (!is_sorted(*a) && \
+			(*a)->elements->data > (*a)->elements->next->data)
 			*a = sa(*a, TRUE);
 		if (!is_sorted(*a) && \
 			(*a)->elements->next->data > (*a)->elements->next->next->data)
