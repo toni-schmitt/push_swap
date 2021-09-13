1c09fa7c752f439f4b65950c865eace4c0709427
Toni Schmitt <tschmitt@student.42heilbronn.de>
2021-09-13 17:14:14 +0200

    hardcode cases for 3 numbers

--- src/sorting/very_small_sort.c
+++ src/sorting/very_small_sort.c
@@ -6,22 +6,37 @@
 /*   By: tschmitt <tschmitt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 12:43:29 by tschmitt          #+#    #+#             */
-/*   Updated: 2021/09/09 18:26:19 by tschmitt         ###   ########.fr       */
+/*   Updated: 2021/09/13 17:12:35 by tschmitt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	very_small_sort(t_stack **a, t_stack **b)
+void	very_small_sort(t_stack **a)
 {
-	(void)b;
-	while (!is_sorted(*a))
+	t_element	*elements[3];
+
+	elements[0] = (*a)->elements;
+	elements[1] = elements[0]->next;
+	elements[2] = elements[1]->next;
+	if (elements[0]->data < elements[1]->data && elements[1]->data > \
+	elements[2]->data && elements[2]->data > elements[0]->data)
 	{
-		if (!is_sorted(*a) && \
-			(*a)->elements->data > (*a)->elements->next->data)
-			*a = sa(*a, TRUE);
-		if (!is_sorted(*a) && \
-			(*a)->elements->next->data > (*a)->elements->next->next->data)
-			*a = rra(*a, TRUE);
+		*a = sa(*a, TRUE);
+		*a = ra(*a, TRUE);
+	}
+	else if (elements[0]->data > elements[1]->data && elements[1]->data < \
+	elements[2]->data && elements[2]->data > elements[0]->data)
+		*a = sa(*a, TRUE);
+	else if (elements[0]->data < elements[1]->data && elements[1]->data > \
+	elements[2]->data && elements[2]->data < elements[0]->data)
+		*a = rra(*a, TRUE);
+	else if (elements[0]->data > elements[1]->data && elements[1]->data < \
+	elements[2]->data && elements[0]->data > elements[2]->data)
+		*a = ra(*a, TRUE);
+	else
+	{
+		*a = ra(*a, TRUE);
+		*a = sa(*a, TRUE);
 	}
 }
