433827b5ba675c47f187e948c1b0b9e63e25ddf2
Toni Schmitt <tschmitt@student.42heilbronn.de>
2021-09-14 20:41:44 +0200

    very_small_sort is now able to sort 2 numbers

--- src/sorting/very_small_sort.c
+++ src/sorting/very_small_sort.c
@@ -6,33 +6,37 @@
 /*   By: tschmitt <tschmitt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 12:43:29 by tschmitt          #+#    #+#             */
-/*   Updated: 2021/09/13 17:12:35 by tschmitt         ###   ########.fr       */
+/*   Updated: 2021/09/14 20:40:36 by tschmitt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+static void	get_data(t_stack **a, int *data)
+{
+	if ((*a)->elements)
+		data[0] = (*a)->elements->data;
+	if ((*a)->elements->next)
+		data[1] = (*a)->elements->next->data;
+	if ((*a)->elements->next->next)
+		data[2] = (*a)->elements->next->next->data;
+}
+
 void	very_small_sort(t_stack **a)
 {
-	t_element	*elements[3];
+	int	data[3];
 
-	elements[0] = (*a)->elements;
-	elements[1] = elements[0]->next;
-	elements[2] = elements[1]->next;
-	if (elements[0]->data < elements[1]->data && elements[1]->data > \
-	elements[2]->data && elements[2]->data > elements[0]->data)
+	get_data(a, data);
+	if (data[0] < data[1] && data[1] > data[2] && data[2] > data[0])
 	{
 		*a = sa(*a, TRUE);
 		*a = ra(*a, TRUE);
 	}
-	else if (elements[0]->data > elements[1]->data && elements[1]->data < \
-	elements[2]->data && elements[2]->data > elements[0]->data)
+	else if (data[0] > data[1] && data[1] < data[2] && data[2] > data[0])
 		*a = sa(*a, TRUE);
-	else if (elements[0]->data < elements[1]->data && elements[1]->data > \
-	elements[2]->data && elements[2]->data < elements[0]->data)
+	else if (data[0] < data[1] && data[1] > data[2] && data[2] < data[0])
 		*a = rra(*a, TRUE);
-	else if (elements[0]->data > elements[1]->data && elements[1]->data < \
-	elements[2]->data && elements[0]->data > elements[2]->data)
+	else if (data[0] > data[1] && data[1] < data[2] && data[0] > data[2])
 		*a = ra(*a, TRUE);
 	else
 	{
